17851d66c1bc4702691ce7c591e40551063289f4
limq <limq@cgnfs.com.cn>
2019-11-22 15:57:54 +0800

    favicon

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module hgin
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/thinkerou/favicon v0.1.0
+)
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/favicon"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(favicon.New("./favicon.ico"))
+	r.LoadHTMLGlob("views/**/*")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -21,5 +24,17 @@ func main() {
 		c.AsciiJSON(http.StatusOK, data)
 	})
 
-	r.Run()
+	r.GET("/demo/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "html demo ~~~~~~~~~~~~~~~",
+		})
+	})
+
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "html demo ~~~~~~~~~~~~~~~",
+		})
+	})
+
+	r.Run(":8080")
 }
